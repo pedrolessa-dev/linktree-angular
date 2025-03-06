2bd067ae8ac031054bd6d2217212a051ac54cd9c
pedrolessa-dev <pedrovideosanimados@gmail.com>
2025-03-05 22:28:03 -0300

    test: verify router-outlet

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('linktree-angular');
   });
 
-  it('should render title', () => {
+  it('should contain a router-outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, linktree-angular');
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 });
